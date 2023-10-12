6d246ff85a4e2e6e7f1ab5a1d884b7ccf79e6361
William Hart <william.hart@maths.ox.ac.uk>
2023-10-12 16:09:26 +0100

    Add test case for minimum with no real arguments

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,3 +16,4 @@ def test_minimum():
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
+    assert ci_course.minimum("spam",1+1j) is None
